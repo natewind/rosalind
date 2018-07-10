226aa1291d3328af8781f4f90316db66f4ac99d8
natewind <sdksing@gmail.com>
2018-07-10 13:07:08 +0300

    Comments: static lib manual

--- biolib.cpp
+++ biolib.cpp
@@ -2,6 +2,13 @@
 
 // TODO: Move long methods here, rename to lib*, build .a, install
 
+// g++ biolib.cpp -o biolib.o -c -std=c++17 -pedantic -Wfatal-errors -O3
+// ar rcs libbio.a biolib.o
+
+// sudo cp libbio.a /usr/local/lib
+
+// g++ main.cpp -lbio -o main.o -std=c++17 -pedantic -Wfatal-errors -O3
+
 int main()
 {
 	// A dummy for make
