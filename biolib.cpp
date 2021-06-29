ce0fd08eeb58c01184a3edebb10da5e25e74a414
natewind <natewind@tuta.io>
2021-06-29 12:07:02 +0300

     

--- biolib.cpp
+++ biolib.cpp
@@ -1,15 +1,4 @@
 #include "biolib.hpp"
 
-// TODO: Move long methods here, rename to lib*, build .a, install
-
-// g++ biolib.cpp -o biolib.o -c -std=c++17 -pedantic -Wfatal-errors -O3
-// ar rcs libbio.a biolib.o
-
-// sudo cp libbio.a /usr/local/lib
-
-// g++ main.cpp -lbio -o main.o -std=c++17 -pedantic -Wfatal-errors -O3
-
-auto main() -> int
-{
-	// A dummy for make
-}
+// A dummy for make
+auto main() -> int {}
