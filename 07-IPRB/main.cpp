501f3b2e596ec0e5228f4adcb363b94176f06482
natewind <natewind@tuta.io>
2021-07-02 02:19:26 +0300

     

--- 07-IPRB/main.cpp
+++ 07-IPRB/main.cpp
@@ -20,11 +20,13 @@ auto main() -> int
 	auto const [RR, Rr, rr] = open("rosalind_iprb.txt").read<int, int, int>();
 
 	auto const total = RR + Rr + rr;
-	auto const total_outcomes = total * (total - 1) / 2;
+	// auto const total_outcomes = total * (total - 1) / 2;
 
-	auto const pure_recessive = rr * (rr - 1) / 2;
-	auto const pure_heterozygous = Rr * (Rr - 1) / 2;
-	auto const recessive_heterozygous = Rr * rr;
+	// auto const pure_recessive = rr * (rr - 1) / 2;
+	// auto const pure_heterozygous = Rr * (Rr - 1) / 2;
+	// auto const recessive_heterozygous = Rr * rr;
+
+	// ATTEMPT 1
 
 	// auto const recessive_outcomes = pure_recessive
 	//                               + 0.25 * double(pure_heterozygous)
@@ -32,11 +34,28 @@ auto main() -> int
 
 	// auto const p_dominant = 1 - recessive_outcomes / total_outcomes;
 
-	auto const recessive_outcomes = 4 * pure_recessive
-	                              + pure_heterozygous
-	                              + 2 * recessive_heterozygous;
+	//=========================================================================
 
-	auto const p_dominant = 1 - recessive_outcomes / (4.0 * total_outcomes);
+	// ATTEMPT 2
+
+	// auto const recessive_outcomes = 4 * pure_recessive
+	//                               + pure_heterozygous
+	//                               + 2 * recessive_heterozygous;
+
+	// auto const p_dominant = (4.0 * total_outcomes - recessive_outcomes) / (4 * total_outcomes);
+
+	//=========================================================================
+
+	// ATTEMPT 3
+
+	auto const p_dominant =
+
+		(long double)
+		(4 * (total * (total - 1) - rr * (rr - 1) - Rr * rr) - Rr * (Rr - 1))
+		/
+		(4 * total * (total - 1));
+
+	// WHAT THE ACTUAL FUCK
 
 	open("result.txt").print(p_dominant);
 }
