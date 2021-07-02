524dd8677de511bd787e49d71a24fc9d39595955
natewind <natewind@tuta.io>
2021-07-02 05:24:56 +0300

     

--- 04-FIB/main.cpp
+++ 04-FIB/main.cpp
@@ -6,5 +6,5 @@
 auto main() -> int
 {
 	auto const [n, k] = open("rosalind_fib.txt").read<int, int>();
-	open("result.txt").print(bio::LucasSequence {1, -k}[n]);
+	open("result.txt").print(bio::LucasSequence{1, -k}[n]);
 }
